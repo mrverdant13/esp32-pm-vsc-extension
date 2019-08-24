c9e9fe199fce0f1ab584d9e2e440fab77bb837c7
Karlo Verde <karlo.verde.1996@gmail.com>
2019-08-24 08:38:44 -0500

    Build Test script

---
+++ assets/scripts/BuildTest.sh
@@ -0,0 +1,9 @@
+rm main/main.cpp
+echo "#include \"test/${testFile}\"" > main/main.cpp
+echo "extern \"C\"" >> main/main.cpp
+echo "{" >> main/main.cpp
+echo "void app_main();" >> main/main.cpp
+echo "}" >> main/main.cpp
+rm -r "build/main"
+clear
+make -j$threadsNumber all
