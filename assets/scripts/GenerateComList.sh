b33f7ebda1ea52a80575e6a1b4171b1ac979cb22
Karlo Verde <karlo.verde.1996@gmail.com>
2019-08-25 00:54:37 -0500

    GenerateComList script added

---
+++ assets/scripts/GenerateComList.sh
@@ -0,0 +1,14 @@
+rm "build/$comPortsFile"
+
+touch "build/$comPortsFile"
+
+line=$(cmd.exe /c mode)
+for comLine in $line ; do
+    if [[ $comLine == *"COM"* ]]; then
+        echo -e "${comLine%:*}" >> "build/$comPortsFile"
+    fi
+done
+
+cat "build/$comPortsFile" > "build/$comPortsFile.txt"
+sleep 1
+rm "build/$comPortsFile"
