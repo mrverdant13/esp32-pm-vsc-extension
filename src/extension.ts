5b3341e6c280070931aaa7f1a0c2f31d2afacda3
Karlo Verde <karlo.verde.1996@gmail.com>
2019-08-26 12:57:44 -0500

    Initialize project command added

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,8 @@ import * as vscode from 'vscode';
 import { lstatSync, readdirSync } from 'fs';
 import { join } from 'path';
 
+import * as utils from './utils';
+
 export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(vscode.commands.registerCommand('extension.create-project', async () => {
@@ -14,9 +16,9 @@ export function activate(context: vscode.ExtensionContext) {
 			canSelectMany: false,
 			openLabel: "Select project location"
 		});
-		if (!projectLocation) { vscode.window.showErrorMessage("Project location not selected"); return; }
 		var useNewWindow = await showQuickPickFrom(["Open in new window", "Open in current window"], "");
 		if (!useNewWindow) { vscode.window.showErrorMessage("Project creation cancelled"); return; }
+		if (!projectLocation) { vscode.window.showErrorMessage("Project location not selected"); return; }
 		introducedName = join(projectLocation[0].fsPath, introducedName);
 		await vscode.workspace.fs.copy(
 			vscode.Uri.file(join(context.extensionPath, "/assets/projectTemplate")),
@@ -26,6 +28,45 @@ export function activate(context: vscode.ExtensionContext) {
 		await vscode.workspace.fs.rename(vscode.Uri.file(join(introducedName, "_vscode")), vscode.Uri.file(join(introducedName, ".vscode")));
 		await vscode.workspace.fs.rename(vscode.Uri.file(join(introducedName, ".vscode/_gitignore")), vscode.Uri.file(join(introducedName, ".vscode/.gitignore")));
 		vscode.commands.executeCommand("vscode.openFolder", vscode.Uri.file(introducedName), useNewWindow.includes("new"));
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('extension.init-project', async () => {
+		var projectLocation = await vscode.window.showOpenDialog({
+			canSelectFiles: false,
+			canSelectFolders: true,
+			canSelectMany: false,
+			openLabel: "Select project folder"
+		});
+		if (!projectLocation) { vscode.window.showErrorMessage("Project location not selected"); return; }
+		if (!await utils.folderExists(vscode.Uri.file(join(projectLocation[0].fsPath, "main"))) || !await utils.fileExists(vscode.Uri.file(join(projectLocation[0].fsPath, "Makefile")))) {
+			vscode.window.showErrorMessage("The folder does not contain an ESP-IDF project");
+			return;
+		}
+		if (!await utils.folderExists(vscode.Uri.file(join(projectLocation[0].fsPath, "main/test")))) {
+			vscode.window.showErrorMessage('The project must use a "test" directory inside the "main" folder.');
+			return;
+		}
+		vscode.window.showWarningMessage("Some files (main/main.c, main/main.cpp, .vscode/settings.js, .vscode/c_cpp_properties.js) may be removed. Do you want to continue?");
+		var removeFiles = await showQuickPickFrom(["Continue", "Cancell"], "Do you wan to continue?");
+		if (!removeFiles) { vscode.window.showErrorMessage("Project opening cancelled"); return; }
+		var useNewWindow = await showQuickPickFrom(["Open in new window", "Open in current window"], "");
+		if (!useNewWindow) { vscode.window.showErrorMessage("Project opening cancelled"); return; }
+		if (removeFiles.includes("Cancell")) { vscode.window.showErrorMessage("Project opening cancelled"); return; }
+		// try {
+			vscode.workspace.fs.delete(vscode.Uri.file(join(projectLocation[0].fsPath, "main/main.c")));
+			vscode.workspace.fs.delete(vscode.Uri.file(join(projectLocation[0].fsPath, "main/main.cpp")));
+		// } catch (error) { }
+		await vscode.workspace.fs.copy(
+			vscode.Uri.file(join(context.extensionPath, "/assets/projectTemplate/_vscode/settings.json")),
+			vscode.Uri.file(join(projectLocation[0].fsPath, ".vscode/settings.json")),
+			{ overwrite: true }
+		);
+		await vscode.workspace.fs.copy(
+			vscode.Uri.file(join(context.extensionPath, "/assets/projectTemplate/_vscode/c_cpp_properties.json")),
+			vscode.Uri.file(join(projectLocation[0].fsPath, ".vscode/c_cpp_properties.json")),
+			{ overwrite: true }
+		);
+		vscode.commands.executeCommand("vscode.openFolder", projectLocation[0], useNewWindow.includes("new"));
 	}));
 
 	function delay(ms: number) {
