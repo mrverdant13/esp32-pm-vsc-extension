b96d7e7f2a8becaf1af941b999d6b5f30a5e6fb1
Karlo Verde <karlo.verde.1996@gmail.com>
2019-08-26 04:39:16 -0500

    create-project command added

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,28 @@ import { join } from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
 
+	context.subscriptions.push(vscode.commands.registerCommand('extension.create-project', async () => {
+		var introducedName = await vscode.window.showInputBox({ prompt: "Name of the new project" });
+		if (!introducedName || introducedName.trim().length === 0) { vscode.window.showErrorMessage("Name project not introduced"); return; }
+		introducedName = introducedName.trim().replace(/ (?= )/gi, '').replace(/ /gi, '-').toLowerCase();
+		var projectLocation = await vscode.window.showOpenDialog({
+			canSelectFiles: false,
+			canSelectFolders: true,
+			canSelectMany: false,
+			openLabel: "Select project location"
+		});
+		if (!projectLocation) { vscode.window.showErrorMessage("Project location not selected"); return; }
+		introducedName = join(projectLocation[0].fsPath, introducedName);
+		await vscode.workspace.fs.copy(
+			vscode.Uri.file(join(context.extensionPath, "/assets/projectTemplate")),
+			vscode.Uri.file(introducedName),
+			{ overwrite: false }
+		);
+		await vscode.workspace.fs.rename(vscode.Uri.file(join(introducedName, "_vscode")), vscode.Uri.file(join(introducedName, ".vscode")));
+		await vscode.workspace.fs.rename(vscode.Uri.file(join(introducedName, ".vscode/_gitignore")), vscode.Uri.file(join(introducedName, ".vscode/.gitignore")));
+		vscode.commands.executeCommand("vscode.openFolder", vscode.Uri.file(introducedName), true);
+	}));
+
 	function delay(ms: number) {
 		return new Promise(resolve => setTimeout(resolve, ms));
 	}
@@ -71,7 +93,7 @@ export function activate(context: vscode.ExtensionContext) {
 		return files;
 	}
 
-	context.subscriptions.push(vscode.commands.registerCommand('extension.buildTest', async () => {
+	context.subscriptions.push(vscode.commands.registerCommand('extension.build-test', async () => {
 		var testFolder: string = 'main/test/';
 		var entryPointPrefix: string = 'main';
 		var entryPointSufixCpp: string = '.cpp';
