ece9115e3138e3b3617822e5658be4e655bca227
Karlo Verde <karlo.verde.1996@gmail.com>
2019-09-29 02:10:19 -0500

    OS checking commented

--- src/extension.ts
+++ src/extension.ts
@@ -35,8 +35,11 @@ import { subprojectsFolder, entryPointPrefix, entryPointExtensions } from './con
 
 export function activate(context: vscode.ExtensionContext) {
 
-	if (process.platform !== 'win32') { vscode.window.showErrorMessage('The "ESP32-PM" extension supports Windows OS only.'); return; }
-	// if (vscode.extensions.getExtension('ms-vscode.cpptools') === undefined) { vscode.window.showErrorMessage('The "ESP32-PM" extension depends on the "C/C++" extension, which is not installed.'); return; }
+	// This extension will register its commands only for Windows.
+	if (process.platform !== 'win32') {
+		vscode.window.showErrorMessage('The "ESP32-PM" extension supports Windows OS only.');
+		return;
+	}
 
 	context.subscriptions.push(vscode.commands.registerCommand('esp32-pm.register-espressif-toolchain', async () => {
 		PathsManager.registerPath(context, PathType.MSYS32);
