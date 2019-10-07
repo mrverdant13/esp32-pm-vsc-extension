130093ab81a4805ed419bde696f02984a7dc6230
Karlo Verde <karlo.verde.1996@gmail.com>
2019-10-06 22:00:48 -0500

    Validation refactored

--- src/esp32-pm-project.ts
+++ src/esp32-pm-project.ts
@@ -16,96 +16,100 @@ import {
     folderExists,
 } from "./utils";
 
-export async function isEspressifProject(projectPath: string): Promise<boolean> {
-
-    // Check if each characteristic file exists.
-    for (let index = 0; index < espressifFiles.length; index++) {
-        const espressifFilePath: string = join(projectPath, espressifFiles[index]);
-        if (!await fileExists(espressifFilePath)) {
-            return false;
-        }
-    }
-
-    // Check if each characteristic folder exists.
-    for (let index = 0; index < espressifFolders.length; index++) {
-        const espressifFolderPath: string = join(projectPath, espressifFolders[index]);
-        if (!await folderExists(espressifFolderPath)) {
-            return false;
-        }
-    }
-
-    // If this point is reached, the project is valid.
-    return true;
+export enum ValidationType {
+    NONE = 0,
+    ESPRESSIF_PROJ = 1,
+    ESP32_PM_PROJ = 2,
 }
 
-export async function isEsp32PmProject(projectPath: string): Promise<boolean> {
-
-    // Check if each characteristic file exists.
-    // and it their :<CONSTANTS>: values has been replaced.
-    for (let index = 0; index < esp32PmFiles.length; index++) {
-        const esp32PmFilePath: string = join(projectPath, esp32PmFiles[index]);
-        if (!await fileExists(esp32PmFilePath)) {
-            return false;
-        }
-        for (let ind = 0; ind < colonConstants.length; ind++) {
-            if ((await vscode.workspace.fs.readFile(vscode.Uri.file(esp32PmFilePath))).toString().includes(':' + colonConstants[ind] + ':')) {
-                return false;
-            }
-        }
-    }
-
-    // Check if each characteristic folder exists.
-    for (let index = 0; index < esp32PmFolders.length; index++) {
-        const esp32PmFolderPath: string = join(projectPath, esp32PmFolders[index]);
-        if (!await folderExists(esp32PmFolderPath)) {
-            return false;
-        }
-    }
-
-    // If this point is reached, the project is valid.
-    return true;
-}
-
-export async function getProjectPath(): Promise<string> {
-
-    // Check if there are no workspace folders.
-    if (vscode.workspace.workspaceFolders === undefined) {
-        throw Error('There is no available workspace.');
-    }
-
-    // Set the project path.
-    const currentProjectPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
-
-
-    // Check if each characteristic file exists.
-    // and it their :<CONSTANTS>: values has been replaced.
-    for (let index = 0; index < esp32PmFiles.length; index++) {
-        const esp32PmFilePath: string = join(currentProjectPath, esp32PmFiles[index]);
-        if (!await fileExists(esp32PmFilePath)) {
-            throw Error('The current workspace does not contain an ESP32-PM project.');
-        }
-        for (let ind = 0; ind < colonConstants.length; ind++) {
-            if ((await vscode.workspace.fs.readFile(vscode.Uri.file(esp32PmFilePath))).toString().includes(':' + colonConstants[ind] + ':')) {
-                throw Error('There are unsolved ESP32-PM project values that has not been set. Execute the "ESP32-PM: Initialize existing project" command.');
-            }
-        }
-    }
-
-    // Check if each characteristic folder exists.
-    for (let index = 0; index < esp32PmFolders.length; index++) {
-        const esp32PmFolderPath: string = join(currentProjectPath, esp32PmFolders[index]);
-        if (!await folderExists(esp32PmFolderPath)) {
-            throw Error('The current workspace does not contain an ESP32-PM project.');
-        }
-    }
-
-    // If this point is reached, the project is valid.
-    return currentProjectPath;
-}
-
-export async function validateProject(): Promise<void> {
+export async function validateEspressifProject(projectPath: string = ''): Promise<void> {
+    const errorMessage: string = 'The selected folder does not contain an Espressif project.';
     try {
-        await getProjectPath();
+        // If the project path is not passed, use the workspace one.
+        if (projectPath.length === 0) {
+            projectPath = await getWorkspacePath(ValidationType.NONE);
+        }
+
+        // Check if each characteristic file exists.
+        for (let index = 0; index < espressifFiles.length; index++) {
+            const espressifFilePath: string = join(projectPath, espressifFiles[index]);
+            if (!await fileExists(espressifFilePath)) {
+                throw Error(errorMessage);
+            }
+        }
+
+        // Check if each characteristic folder exists.
+        for (let index = 0; index < espressifFolders.length; index++) {
+            const espressifFolderPath: string = join(projectPath, espressifFolders[index]);
+            if (!await folderExists(espressifFolderPath)) {
+                throw Error(errorMessage);
+            }
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function validateEsp32PmProject(projectPath: string = ''): Promise<void> {
+    const isNotEsp32PmProjectMessage: string = 'The current workspace does not contain an ESP32-PM project.';
+    const constantValuesUnsolved: string = 'There are unsolved ESP32-PM project values that has not been set. Execute the "ESP32-PM: Initialize existing project" command.';
+
+    try {
+        // If the project path is not passed, use the workspace one.
+        if (projectPath.length === 0) {
+            projectPath = await getWorkspacePath(ValidationType.ESPRESSIF_PROJ);
+        }
+
+        // Check if each characteristic file exists.
+        // and it their :<CONSTANTS>: values has been replaced.
+        for (let index = 0; index < esp32PmFiles.length; index++) {
+            const esp32PmFilePath: string = join(projectPath, esp32PmFiles[index]);
+            if (!await fileExists(esp32PmFilePath)) {
+                throw Error(isNotEsp32PmProjectMessage);
+            }
+            for (let ind = 0; ind < colonConstants.length; ind++) {
+                if ((await vscode.workspace.fs.readFile(vscode.Uri.file(esp32PmFilePath))).toString().includes(':' + colonConstants[ind] + ':')) {
+                    throw Error(constantValuesUnsolved);
+                }
+            }
+        }
+
+        // Check if each characteristic folder exists.
+        for (let index = 0; index < esp32PmFolders.length; index++) {
+            const esp32PmFolderPath: string = join(projectPath, esp32PmFolders[index]);
+            if (!await folderExists(esp32PmFolderPath)) {
+                throw Error(isNotEsp32PmProjectMessage);
+            }
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function getWorkspacePath(validationType: ValidationType): Promise<string> {
+    try {
+        // Check if there are no workspace folders.
+        if (vscode.workspace.workspaceFolders === undefined) {
+            throw Error('There is no available workspace.');
+        }
+
+        // Get the workspace path.
+        const workspacePath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+
+        // Validate if necessary.
+        switch (validationType) {
+            case ValidationType.ESPRESSIF_PROJ:
+                await validateEspressifProject(workspacePath);
+                break;
+            case ValidationType.ESP32_PM_PROJ:
+                await validateEsp32PmProject(workspacePath);
+                break;
+            default:
+                break;
+        }
+
+        // Return the workspace path.
+        return workspacePath;
     } catch (error) {
         throw error;
     }
