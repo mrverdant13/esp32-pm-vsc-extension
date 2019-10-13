2b1d1bbc75886189d8ddde6d1605613f7265dd1d
Karlo Verde <karlo.verde.1996@gmail.com>
2019-10-12 23:43:09 -0500

    Using exceptions

--- src/utils.ts
+++ src/utils.ts
@@ -58,34 +58,42 @@ export async function pickElement(elements: Array<string>, hint: string, errorMe
 }
 
 export async function pickFolder(prompt: string, errorMessage: string): Promise<string> {
-    // Show a custom folder selection folder.
-    const selectedFolder: Array<vscode.Uri> | undefined = await vscode.window.showOpenDialog({
-        canSelectFiles: false,
-        canSelectFolders: true,
-        canSelectMany: false,
-        openLabel: prompt
-    });
+    try {
+        // Show a custom folder selection folder.
+        const selectedFolder: Array<vscode.Uri> | undefined = await vscode.window.showOpenDialog({
+            canSelectFiles: false,
+            canSelectFolders: true,
+            canSelectMany: false,
+            openLabel: prompt
+        });
 
-    // If no folder was selected, throw an error.
-    if (selectedFolder === undefined) {
-        throw Error(errorMessage);
+        // If no folder was selected, throw an error.
+        if (selectedFolder === undefined) {
+            throw Error(errorMessage);
+        }
+
+        // Return the selected folder.
+        return selectedFolder[0].fsPath;
+    } catch (error) {
+        throw error;
     }
-
-    // Return the selected folder.
-    return selectedFolder[0].fsPath;
 }
 
 export async function introduceString(prompt: string, errorMessage: string) {
-    // Show a custom text input.
-    const newProjectName: string | undefined = await vscode.window.showInputBox({ prompt: prompt });
+    try {
+        // Show a custom text input.
+        const newProjectName: string | undefined = await vscode.window.showInputBox({ prompt: prompt });
 
-    // If no text was introduced, throw an error.
-    if (newProjectName === undefined || newProjectName.trim().length === 0) {
-        throw Error(errorMessage);
+        // If no text was introduced, throw an error.
+        if (newProjectName === undefined || newProjectName.trim().length === 0) {
+            throw Error(errorMessage);
+        }
+
+        // Return the introduced string.
+        return newProjectName;
+    } catch (error) {
+        throw error;
     }
-
-    // Return the introduced string.
-    return newProjectName;
 }
 
 async function elementExists(path: string, type: vscode.FileType): Promise<boolean> {
@@ -102,67 +110,114 @@ async function elementExists(path: string, type: vscode.FileType): Promise<boole
 }
 
 export async function folderExists(path: string): Promise<boolean> {
-    // Return the revision of the folder existence.
-    return await elementExists(path, vscode.FileType.Directory);
+    try {
+        // Return the revision of the folder existence.
+        return await elementExists(path, vscode.FileType.Directory);
+    } catch (error) {
+        throw error;
+    }
 }
 
 export async function fileExists(path: string): Promise<boolean> {
-    // Return the revision of the file existence.
-    return await elementExists(path, vscode.FileType.File);
-}
-
-export function getFolders(path: string): string[] {
-    // Get the elements inside the passed folder and filter the directories.
-    return readdirSync(path).filter((element) => {
-        if (lstatSync(join(path, element)).isDirectory()) {
-            return element;
-        }
-    });
-}
-
-export function getFiles(path: string): string[] {
-    // Get the elements inside the passed folder and filter the files.
-    return readdirSync(path).filter((element) => {
-        if (lstatSync(join(path, element)).isFile()) {
-            return element;
-        }
-    });
-}
-
-export async function filterExistingFolders(folders: string[]): Promise<string[]> {
-    var existingPaths: string[] = [];
-
-    // Check if each of the passed folders exist and, if so, append them to a final array.
-    for (var index: number = 0; index < folders.length; index++) {
-        if (await folderExists(folders[index])) {
-            existingPaths.push(folders[index]);
-        }
+    try {
+        // Return the revision of the file existence.
+        return await elementExists(path, vscode.FileType.File);
+    } catch (error) {
+        throw error;
     }
-
-    // Return the resulting array.
-    return existingPaths;
 }
 
-export function executeShellCommands(name: string, commandLines: string[]): void {
+export function getFolders(path: string): Array<string> {
+    try {
+        // Get the elements inside the passed folder and filter the directories.
+        return readdirSync(path).filter((element) => {
+            if (lstatSync(join(path, element)).isDirectory()) {
+                return element;
+            }
+        });
+    } catch (error) {
+        throw error;
+    }
+}
 
-    // Create a task related to a terminal which will execute the passed commands.
-    const task = new vscode.Task(
-        { type: "shell" },
-        vscode.TaskScope.Workspace,
-        name,
-        "ESP32-PM",
-        new vscode.ShellExecution(commandLines.join(" && "))
-    );
+export function getFiles(path: string): Array<string> {
+    try {
+        // Get the elements inside the passed folder and filter the files.
+        return readdirSync(path).filter((element) => {
+            if (lstatSync(join(path, element)).isFile()) {
+                return element;
+            }
+        });
+    } catch (error) {
+        throw error;
+    }
+}
 
-    // The executed commands will not be printed.
-    task.presentationOptions.echo = false;
+export async function filterExistingFolders(folders: Array<string>): Promise<Array<string>> {
+    try {
+        var existingPaths: Array<string> = [];
 
-    // The terminal will be first cleared.
-    task.presentationOptions.clear = true;
+        // Check if each of the passed folders exist and, if so, append them to a final array.
+        for (var index: number = 0; index < folders.length; index++) {
+            if (await folderExists(folders[index])) {
+                existingPaths.push(folders[index]);
+            }
+        }
 
-    // The generated terminal will take focus.
-    task.presentationOptions.focus = true;
+        // Return the resulting array.
+        return existingPaths;
+    } catch (error) {
+        throw error;
+    }
+}
 
-    // Execute the task.
-    vscode.tasks.executeTask(task);
+export async function copyFile(originFilePath: string, destinationFilePath: string): Promise<void> {
+    try {
+        await vscode.workspace.fs.copy(
+            vscode.Uri.file(originFilePath),
+            vscode.Uri.file(destinationFilePath),
+            { overwrite: true }
+        );
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function replaceInFile(filePath: string, find: RegExp, replace: string): Promise<void> {
+    try {
+        const fileContent: string = (await vscode.workspace.fs.readFile(vscode.Uri.file(filePath))).toString();
+        await vscode.workspace.fs.writeFile(
+            vscode.Uri.file(filePath),
+            Buffer.from(fileContent.replace(find, replace))
+        );
+    } catch (error) {
+        throw error;
+    }
+}
+
+export function executeShellCommands(name: string, commandLines: Array<string>): void {
+    try {
+        // Create a task related to a terminal which will execute the passed commands.
+        const task = new vscode.Task(
+            { type: "shell" },
+            vscode.TaskScope.Workspace,
+            name,
+            "ESP32-PM",
+            new vscode.ShellExecution(commandLines.join(" && "))
+        );
+
+        // The executed commands will not be printed.
+        task.presentationOptions.echo = false;
+
+        // The terminal will be first cleared.
+        task.presentationOptions.clear = true;
+
+        // The generated terminal will take focus.
+        task.presentationOptions.focus = true;
+
+        // Execute the task.
+        vscode.tasks.executeTask(task);
+    } catch (error) {
+        throw error;
+    }
 }
