6d36c466921d9b8abbc45d40a76038f80849ad40
Karlo Verde <karlo.verde.1996@gmail.com>
2019-10-22 04:11:41 -0500

    Characteristic folder for tensa dir

---
+++ src/constants/xtensa.ts
@@ -0,0 +1,10 @@
+export namespace Paths {
+    export const Folders: Array<string> = [
+        'bin/',
+        'include/',
+        'lib/',
+        'libexec/',
+        'share/',
+        'xtensa-esp32-elf/',
+    ];
+}
